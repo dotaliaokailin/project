eaebd13074b6826510be65ce50c39d6d865ed808
dotaliaokailin <719098033@qq.com>
2020-10-29 14:30:26 +0800

    手机邮箱校验

--- vue_test/src/views/user/AddUser.vue
+++ vue_test/src/views/user/AddUser.vue
@@ -122,6 +122,40 @@
       }
     },
     data() {
+      //邮箱校验
+      let checkEmail = (rule, value, callback) => {
+        const mailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+        if (!value) {
+          return callback(new Error("邮箱不能为空"));
+        }
+        setTimeout(() => {
+          if (mailReg.test(value)) {
+            callback();
+          } else {
+            callback(new Error("请输入正确的邮箱格式"));
+          }
+        }, 100);
+      };
+      //手机校验
+      let checkPhone = (rule, value, callback) => {
+        const phoneReg = /^1[34578]\d{9}$$/;
+        if (!value) {
+          return callback(new Error("电话号码不能为空"));
+        }
+        setTimeout(() => {
+          // Number.isInteger是es6验证数字是否为整数的方法,实际输入的数字总是识别成字符串
+          // 所以在前面加了一个+实现隐式转换
+          if (!Number.isInteger(+value)) {
+            callback(new Error("请输入数字值"));
+          } else {
+            if (phoneReg.test(value)) {
+              callback();
+            } else {
+              callback(new Error("电话号码格式不正确"));
+            }
+          }
+        }, 100);
+      };
       return {
         // 控制弹出框显示隐藏
         showDialog:false,
@@ -161,12 +195,12 @@
           }],
           email: [{
             required: true,
-            message: '请输入邮箱',
+            validator: checkEmail,
             trigger: 'blur'
           }],
           phoneNumber: [{
             required: true,
-            message: '请输入手机号',
+            validator: checkPhone,
             trigger: 'blur'
           }],
           birth: [{
