917a1bb578374e062512e27203c468a9972913cc
dotaliaokailin <719098033@qq.com>
2020-11-05 17:12:28 +0800

    poi

---
+++ test/poi/src/main/java/com/liao/easyExcel/EasyTest.java
@@ -0,0 +1,112 @@
+package com.liao.easyExcel;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelReader;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.read.metadata.ReadSheet;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.*;
+
+public class EasyTest {
+    static final String PAHT = "D:/git_project/test/poi/";
+
+    private List<DemoData> data() {
+        List<DemoData> list = new ArrayList<DemoData>();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+    /**
+     * 最简单的写
+     * <p>1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>2. 直接写即可
+     */
+    @Test
+    public void simpleWrite() {
+        // 写法1
+        String fileName = PAHT +  "easyWrite.xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, DemoData.class).sheet("模板").doWrite(data());
+    }
+
+    /**
+     * 最简单的读
+     * <p>1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>2. 由于默认一行行的读取excel，所以需要创建excel一行一行的回调监听器，参照{@link DemoDataListener}
+     * <p>3. 直接读即可
+     */
+    @Test
+    public void simpleRead() {
+        // 有个很重要的点 DemoDataListener 不能被spring管理，要每次读取excel都要new,然后里面用到spring可以构造方法传进去
+        // 写法1
+        String fileName = PAHT +  "easyWrite.xlsx";
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
+        EasyExcel.read(fileName, DemoData.class, new DemoDataListener()).sheet().doRead();
+    }
+
+    /**
+     * 根据参数只导出指定列
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>
+     * 2. 根据自己或者排除自己需要的列
+     * <p>
+     * 3. 直接写即可
+     *
+     * @since 2.1.1
+     */
+    @Test
+    public void excludeOrIncludeWrite() {
+        String fileName = PAHT + "excludeOrIncludeWrite" + System.currentTimeMillis() + ".xlsx";
+
+        // 根据用户传入字段 假设我们要忽略 date
+        Set<String> excludeColumnFiledNames = new HashSet<String>();
+        excludeColumnFiledNames.add("date");
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName, DemoData.class).excludeColumnFiledNames(excludeColumnFiledNames).sheet("模板")
+                .doWrite(data());
+
+        fileName = PAHT + "excludeOrIncludeWrite" + System.currentTimeMillis() + ".xlsx";
+        // 根据用户传入字段 假设我们只要导出 date
+        Set<String> includeColumnFiledNames = new HashSet<String>();
+        includeColumnFiledNames.add("date");
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName, DemoData.class).includeColumnFiledNames(includeColumnFiledNames).sheet("模板")
+                .doWrite(data());
+    }
+
+    /**
+     * 指定写入的列
+     * <p>1. 创建excel对应的实体对象 参照{@link IndexData}
+     * <p>2. 使用{@link ExcelProperty}注解指定写入的列
+     * <p>3. 直接写即可
+     */
+    @Test
+    public void indexWrite() {
+        String fileName = PAHT + "indexWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName, IndexData.class).sheet("模板").doWrite(data());
+    }
+
+    /**
+     * 复杂头写入
+     * <p>1. 创建excel对应的实体对象 参照{@link ComplexHeadData}
+     * <p>2. 使用{@link ExcelProperty}注解指定复杂的头
+     * <p>3. 直接写即可
+     */
+    @Test
+    public void complexHeadWrite() {
+        String fileName = PAHT + "complexHeadWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName, ComplexHeadData.class).sheet("模板").doWrite(data());
+    }
+}
